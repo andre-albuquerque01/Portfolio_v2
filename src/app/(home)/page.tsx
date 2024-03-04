fc9dcb7fab58c9802f790a440af588fa3d581701
andre <andrealbuquerque2001@gmail.com>
2024-03-04 12:00:00 -0300

    Sua mensagem de commit aqui

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center text-center flex-col gap-5 text-white h-[700px]">
+    <main className="flex justify-center items-center text-center flex-col gap-5 text-white h-[700px] p-2">
       <h1 className="text-2xl font-bold">HEY, I AM ANDRÉ ALBUQUERQUE</h1>
-      <p>
+      <p className="">
         A full-stack developer focused on web Applications, building web
         Applications to facilitate life.
       </p>
