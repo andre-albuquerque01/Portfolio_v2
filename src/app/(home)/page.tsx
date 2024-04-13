e8d8aa6421c5205afa6398ec89cf84bb687c7a58
andre <andrealbuquerque2001@gmail.com>
2024-04-13 12:00:00 -0300

    Seu commit para amanhã

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main className="">
-      Brsa <div>asdasd</div>
+    <main className="flex justify-center items-center flex-col gap-5 text-white h-[700px]">
+      <h1 className="text-2xl font-bold">HEY, I AM ANDRÉ ALBUQUERQUE</h1>
+      <p>
+        A full-stack developer focused on web Applications, building web
+        Applications to facilitate life.
+      </p>
+      <Link href="/about" className="px-4 py-2 bg-blue-500 rounded-md">
+        About me
+      </Link>
     </main>
   )
 }
