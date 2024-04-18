3fe08e8343e9f0811d10a92b6c76febf3e0d164a
andre <andrealbuquerque2001@gmail.com>
2024-04-18 09:49:00 -0300

    feat: first update

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { fontBody } from './fonts/fonts'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'André',
+  description: 'Desmonstrar minhas habilidades',
+  authors: {
+    name: 'André Albuquerque',
+    url: 'https://ae.dev.br/',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`bg-zinc-700 antialiased ${inter.className} ${fontBody.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
