1447d93015371675c25c1d6b2fcf82c89fd957d5
andre <andrealbuquerque2001@gmail.com>
2024-04-19 12:31:36 -0300

    feat: create new pages

---
+++ src/app/(home)/projects/apiUser/page.tsx
@@ -0,0 +1,96 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function ApiUser() {
+  const pathName = '/api-spring/imgApi_V1'
+  return (
+    <aside className="max-w-[1200px] mx-auto text-justify mt-10 mb-5 text-white max-sm:px-2 px-20 space-y-8">
+      <div className="" id="apiUser">
+        <div className="sobre">
+          <div className="text-2xl">
+            <h1>Api User</h1>
+          </div>
+          <div className="text-justify">
+            Um sistema eficiente para consumir API, desenvolvido utilizando o
+            framework Spring Boot em conjunto com o MongoDB. Este sistema foi
+            cuidadosamente construído para oferecer uma fluidez, permitindo a
+            interação com diversas API por meio da poderosa tecnologia Spring
+            Boot, combinada com a flexibilidade e escalabilidade do MongoDB.
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className="space-y-8">
+          <div className="space-y-2">
+            <div className="text-xl">
+              <h2 className="font-bold">Visão Geral</h2>
+            </div>
+            <div className="font-semibold">O produto:</div>
+            <div className="text-justify">
+              API desenvolvida utilizando Spring Boot e MongoDB, prontamente
+              consumida no ambiente React com TypeScript.
+            </div>
+            <div className="font-semibold">O objetivo:</div>
+            <div className="text-justify">
+              O objetivo é proporcionar uma experiência de aprendizado
+              abrangente sobre como consumir APIs, oferecendo insights valiosos
+              e conhecimentos práticos nesse processo essencial para o
+              desenvolvimento de software.
+            </div>
+            <div className="font-semibold">Minha função:</div>
+            <div className="text-justify">Desenvolvedor Full Stack</div>
+          </div>
+
+          <div className="space-y-2">
+            <div className="text-xl">
+              <h2 className="font-bold">Visão</h2>
+            </div>
+            <div className="font-semibold">Imagens do resultado:</div>
+            <div className="flex flex-wrap space-y-4">
+              <Image
+                src={`${pathName}/index.png`}
+                width={1020}
+                height={700}
+                alt="tela do projeto"
+              />
+              <Image
+                src={`${pathName}/Cad.png`}
+                width={820}
+                height={550}
+                alt="tela do projeto"
+              />
+              <Image
+                src={`${pathName}/Alt.png`}
+                width={820}
+                height={550}
+                alt="tela do projeto"
+              />
+            </div>
+
+            <div className="font-semibold">Sobre:</div>
+            <div className="descriacao">
+              O sistema utiliza uma API para registrar usuários, solicitando
+              informações como nome e email. Esses dados são então inseridos no
+              banco de dados por meio da API local.
+            </div>
+
+            <div className="font-semibold">Repositório:</div>
+            <div className="descriacao">
+              O repositório está alocado no GitHub.
+            </div>
+            <div className="descriacao">
+              <Link
+                href="https://github.com/andre-albuquerque01/learn_SpringBoot/tree/main/ReactTs_Api_SpringBoot_User"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-orange-500"
+              >
+                Link para o repositório
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
+  )
+}
---
+++ src/app/(home)/projects/page.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/no-unescaped-entities */
+import { CardProject } from '@/components/cardProjects'
+
+export default function Contact() {
+  return (
+    <div className="max-w-[1200px] min-h-screen mx-auto mb-5">
+      <div>
+        <h1 className="uppercase text-white text-2xl text-center mt-10">
+          PROJECTS
+        </h1>
+        <div className="w-20 h-1 rounded-sm bg-blue-600 mt-2 mx-auto"></div>
+      </div>
+      <div>
+        <p className="text-white text-md text-center mt-7">
+          Here you will find some of the principal personal projects that I have
+          created
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-5 mt-10">
+        <CardProject
+          pathLink={'tutuFood'}
+          titleLink={'Operational Management for restaurants'}
+          pathImage={''}
+          title={'Operational Management for restaurants'}
+          text={
+            'Menu system with order management, table management, item management, and category management.'
+          }
+        />
+        <CardProject
+          pathLink={'event'}
+          titleLink={'Sistema de eventos'}
+          pathImage={'/event/event.png'}
+          title={'Events'}
+          text={
+            'Events website, for a specific organization, to promote its events.'
+          }
+        />
+        <CardProject
+          pathLink={'operationalManagement'}
+          titleLink={'Operational Management'}
+          pathImage={'/managemenSystem/Gerenciamento.png'}
+          title={'Operational Management'}
+          text={
+            'Operational Management of companies providing outsourced labor services.'
+          }
+        />
+        <CardProject
+          pathLink={'takeThePhone'}
+          titleLink={'E-commerce for restaurants'}
+          pathImage={'/e-commerceRestaurants/takeThePhone.jpg'}
+          title={'E-commerce for restaurants'}
+          text={'Digital menu with online ordering.'}
+        />
+
+        <CardProject
+          pathLink={'apiUser'}
+          titleLink={'Api User'}
+          pathImage={'/api-spring/ApiUser.png'}
+          title={'Api User'}
+          text={
+            'A study on how to consume APIs and create an API using Spring Boot with MongoDB.'
+          }
+        />
+      </div>
+    </div>
+  )
+}
