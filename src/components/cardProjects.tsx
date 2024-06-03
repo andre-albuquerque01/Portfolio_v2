4686e5692c88112e8b3710546978575d6c071d85
andre <andrealbuquerque2001@gmail.com>
2024-06-03 16:48:56 -0300

    feat: update component

--- src/components/cardProjects.tsx
+++ src/components/cardProjects.tsx
@@ -32,7 +32,7 @@ export const CardProject = ({
       />
       <div className="flex flex-col items-center gap-6 w-[50%] max-md:w-full max-md:px-2 max-md:mb-2 max-md:text-center">
         <h2 className="text-lg font-semibold">{title}</h2>
-        <p>{text}</p>
+        <p className="text-center">{text}</p>
       </div>
     </Link>
   )
