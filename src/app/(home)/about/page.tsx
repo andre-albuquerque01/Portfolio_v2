a8feccf145496577b721a91ae20daa7ffa2a4304
andre <andrealbuquerque2001@gmail.com>
2024-06-08 18:32:30 -0300

    feat: update page

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -2,6 +2,8 @@ import { Certification } from '@/components/certification'
 import { Experience } from '@/components/experience'
 import { Skills } from '@/components/skills'
 import { Metadata } from 'next'
+import Link from 'next/link'
+import { GoDownload } from 'react-icons/go'
 
 /* eslint-disable react/no-unescaped-entities */
 export const metadata: Metadata = {
@@ -14,15 +16,25 @@ export default function About() {
         <h1 className="uppercase text-white text-2xl text-center mt-10">
           About me
         </h1>
-        <div className="w-20 h-1 rounded-sm bg-blue-600 mt-2 mx-auto"></div>
+        <div className="w-20 h-1 rounded-sm bg-blue-600 mt-1 mx-auto"></div>
       </div>
       <div className="text-justify">
-        <p className="text-white text-md text-center mt-7">
+        <p className="text-white text-md text-center mt-3">
           Here you will find more information about me, what I do, and my
           current skills mostly in terms of programming and technology.
         </p>
       </div>
-      <div className="text-white text-justify space-x-4 space-y-2 mt-3">
+      <div className="flex mt-2" title="Download do currículo">
+        <Link
+          href="/docs/Profile.pdf"
+          passHref
+          download="Profile.pdf"
+          className="inline-block mx-auto"
+        >
+          <GoDownload className="w-5 h-5 text-white" />
+        </Link>
+      </div>
+      <div className="text-white text-justify space-x-4 space-y-2 mt-2">
         <h1 className="text-xl">Get to know me!</h1>
         <p>
           A full-stack developer focused on web applications, building web
