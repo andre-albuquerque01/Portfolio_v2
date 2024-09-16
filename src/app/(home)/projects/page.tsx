4cfe42f67c277adfc16f9422317e131c3e209a1c
andre <andrealbuquerque2001@gmail.com>
2024-09-16 11:34:11 -0300

    feat: update page

--- src/app/(home)/projects/page.tsx
+++ src/app/(home)/projects/page.tsx
@@ -3,101 +3,116 @@ import { CardProject } from '@/components/cardProjects'
 
 export default function Contact() {
   return (
-    <div className="max-w-[1200px] min-h-screen mx-auto mb-5">
-      <div>
-        <h1 className="uppercase text-white text-2xl text-center mt-10">
-          PROJECTS
-        </h1>
-        <div className="w-20 h-1 rounded-sm bg-blue-600 mt-2 mx-auto"></div>
-      </div>
-      <div>
-        <p className="text-white text-md text-center mt-7">
-          Here you will find some of the principal personal projects that I have
-          created
-        </p>
-      </div>
-      <div className="flex flex-col items-center justify-center gap-5 mt-10">
-        <CardProject
-          pathLink={'imobiliaria'}
-          titleLink={'Real Estate'}
-          pathImage={'/imobiliaria/capa.png'}
-          title={'Real Estate'}
-          text={
-            'An app for business use, specifically designed for Real Estate.'
-          }
-        />
-        <CardProject
-          pathLink={'class'}
-          titleLink={'Classroom Management'}
-          pathImage={'/class/home.png'}
-          title={'Classroom Management'}
-          text={'Student Management System Developed for the challenge'}
-        />
-        <CardProject
-          pathLink={'workOut'}
-          titleLink={'Work Out'}
-          pathImage={'/workOut/home.png'}
-          title={'Work out'}
-          text={'An app for training for personal use.'}
-        />
-        <CardProject
-          pathLink={'techCraft'}
-          titleLink={'Landing Page'}
-          pathImage={'/techCraft/home.png'}
-          title={'Landing Page'}
-          text={'Landing page for business'}
-        />
-        <CardProject
-          pathLink={'socialMedia'}
-          titleLink={'Social media'}
-          pathImage={'/socialMedia/login.jpg'}
-          title={'Social media'}
-          text={'System for business intern, to enhance human relationships'}
-        />
-        <CardProject
-          pathLink={'tutuFood'}
-          titleLink={'Operational Management for restaurants'}
-          pathImage={'/tutuFood/tutufoodCapa.png'}
-          title={'Operational Management for restaurants'}
-          text={
-            'Menu system with order management, table management, item management, and category management.'
-          }
-        />
-        <CardProject
-          pathLink={'event'}
-          titleLink={'Sistema de eventos'}
-          pathImage={'/event/event.png'}
-          title={'Events'}
-          text={
-            'Events website, for a specific organization, to promote its events.'
-          }
-        />
-        <CardProject
-          pathLink={'operationalManagement'}
-          titleLink={'Operational Management'}
-          pathImage={'/managemenSystem/Gerenciamento.png'}
-          title={'Operational Management'}
-          text={
-            'Operational Management of companies providing outsourced labor services.'
-          }
-        />
-        <CardProject
-          pathLink={'takeThePhone'}
-          titleLink={'E-commerce for restaurants'}
-          pathImage={'/e-commerceRestaurants/takeThePhone.jpg'}
-          title={'E-commerce for restaurants'}
-          text={'Digital menu with online ordering.'}
-        />
+    <div
+      style={{
+        backgroundImage:
+          'linear-gradient(to bottom, #00000076, #000000eb), url(/pages/backgroundProject.jpg)',
+        filter: 'brightness(150%)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: 'calc(100vh - 80px)',
+        minWidth: '100%',
+      }}
+    >
+      <div className="max-w-[1200px] min-h-screen mx-auto pb-5">
+        <div>
+          <h1 className="uppercase text-white text-2xl text-center pt-10">
+            PROJECTS
+          </h1>
+          <div className="w-20 h-1 rounded-sm bg-blue-600 mt-2 mx-auto"></div>
+        </div>
+        <div>
+          <p className="text-white text-md text-center mt-7">
+            {/* Here you will find some of the principal personal projects that I
+            have created */}
+            Aqui encontrará alguns dos principais projetos que foram feitos
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-5 mt-10">
+          <CardProject
+            pathLink={'imobiliaria'}
+            titleLink={'Imobiliária'}
+            pathImage={'/imobiliaria/capa.png'}
+            title={'Imobiliária'}
+            text={
+              'Um sistema para imobiliária, onde pode publicar seus anúncios'
+            }
+          />
+          <CardProject
+            pathLink={'class'}
+            titleLink={'Sistema de gerenciamento de classe'}
+            pathImage={'/class/home.png'}
+            title={'Sistema de gerenciamento de classe'}
+            text={'Sistema com objetivo de gerenciamento de alunos nas turmas'}
+          />
+          <CardProject
+            pathLink={'workOut'}
+            titleLink={'Treino'}
+            pathImage={'/workOut/home.png'}
+            title={'Treino'}
+            text={'Sistema para anotar os exercícios'}
+          />
+          <CardProject
+            pathLink={'techCraft'}
+            titleLink={'Landing Page'}
+            pathImage={'/techCraft/home.png'}
+            title={'Landing Page'}
+            text={'Landing page para empresa'}
+          />
+          <CardProject
+            pathLink={'socialMedia'}
+            titleLink={'Social media'}
+            pathImage={'/socialMedia/login.jpg'}
+            title={'Social media'}
+            text={
+              'Rede social para empresa, para melhorar a interação dos funcionários'
+            }
+          />
+          <CardProject
+            pathLink={'tutuFood'}
+            titleLink={'Sistema de cardápio'}
+            pathImage={'/tutuFood/tutufoodCapa.png'}
+            title={'Sistema de cardápio'}
+            text={
+              'Sistema de gerenciamento de pedidos, para controle da empresa e do cliente'
+            }
+          />
+          <CardProject
+            pathLink={'event'}
+            titleLink={'Sistema de eventos'}
+            pathImage={'/event/event.png'}
+            title={'Events'}
+            text={
+              'Eventos website, para uma específica organização, para promover eventos.'
+            }
+          />
+          <CardProject
+            pathLink={'operationalManagement'}
+            titleLink={'Sistema de gerenciamento de escalas'}
+            pathImage={'/managemenSystem/Gerenciamento.png'}
+            title={'Sistema de gerenciamento de escalas'}
+            text={
+              'Sistema para gerenciamento de escalas de funcionários de uma determinda organização'
+            }
+          />
+          <CardProject
+            pathLink={'takeThePhone'}
+            titleLink={'E-commerce para restaurantes'}
+            pathImage={'/e-commerceRestaurants/takeThePhone.jpg'}
+            title={'E-commerce para restaurantes'}
+            text={'Digital menu with online ordering.'}
+          />
 
-        <CardProject
-          pathLink={'apiUser'}
-          titleLink={'Api User'}
-          pathImage={'/api-spring/ApiUser.png'}
-          title={'Api User'}
-          text={
-            'A study on how to consume APIs and create an API using Spring Boot with MongoDB.'
-          }
-        />
+          <CardProject
+            pathLink={'apiUser'}
+            titleLink={'Api User'}
+            pathImage={'/api-spring/ApiUser.png'}
+            title={'Api User'}
+            text={
+              'A study on how to consume APIs and create an API using Spring Boot with MongoDB.'
+            }
+          />
+        </div>
       </div>
     </div>
   )
