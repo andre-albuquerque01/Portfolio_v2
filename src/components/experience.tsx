2286863390917349afc13dcf9d16e136cec45921
andre <andrealbuquerque2001@gmail.com>
2024-09-16 11:34:21 -0300

    feat: update component

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,28 +1,29 @@
 /* eslint-disable react/no-unescaped-entities */
 export const Experience = () => {
   return (
-    <div className="text-white text-justify space-x-4 space-y-2 mt-3">
-      <h1 className="text-xl">Experience</h1>
+    <div className="text-zinc-400 text-justify space-x-4 space-y-2 mt-3">
+      <h1 className="text-xl">Experiência</h1>
       <div className="space-x-8">
         <p>
-          Internship at Public Defender's Office - Front-end Developer - 08/2023
-          - 12/2023
+          Estágio na Defensoria Pública - Desenvolvedor Front-end - 08/2023 -
+          12/2023
         </p>
         <span>
-          During my internship at the Public Defender's Office, I worked as a
-          Front-end Developer, actively contributing to the development team.
+          Durante meu estágio na Defensoria Pública, trabalhei como
+          Desenvolvedor Front-end, contribuindo ativamente para a equipe de
+          desenvolvimento.
         </span>
       </div>
       <div className="space-x-8">
         <p>
-          Internship at Caixa Economica - Testing assistant - 12/2022 - 08/2023
+          Estágio na Caixa Econômica - Assistente de Testes - 12/2022 - 08/2023
         </p>
         <span>
-          During my internship at Caixa Econômica, I worked as a test assistant,
-          actively contributing to the development team. My journey was from
-          December 2022 to August 2023, during which I gained valuable insights
-          and developed practical skills in the field of software testing and
-          quality assurance.
+          Durante meu estágio na Caixa Econômica, trabalhei como assistente de
+          testes, contribuindo ativamente para a equipe de desenvolvimento.
+          Minha jornada foi de dezembro de 2022 a agosto de 2023, durante a qual
+          adquiri insights valiosos e desenvolvi habilidades práticas na área de
+          testes de software e garantia de qualidade.
         </span>
       </div>
     </div>
