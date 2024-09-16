e715ced7799388a32ea59fe4c90cd67161790438
andre <andrealbuquerque2001@gmail.com>
2024-09-16 11:34:17 -0300

    feat: update component

--- src/components/cardProjects.tsx
+++ src/components/cardProjects.tsx
@@ -19,7 +19,7 @@ export const CardProject = ({
   return (
     <Link
       href={`/projects/${pathLink}`}
-      className="w-[90%]  h-52 flex items-center justify-around gap-5 max-md:flex-col max-md:h-auto max-md:justify-end text-white border border-white rounded-xl transform duration-700 hover:bg-zinc-700 hover:scale-110"
+      className="w-[90%] h-52 flex items-center justify-around gap-5 max-md:flex-col max-md:h-auto max-md:justify-end text-white border border-white rounded-xl transform duration-700 hover:bg-zinc-700 hover:scale-110"
       title={titleLink}
     >
       <Image
@@ -27,7 +27,7 @@ export const CardProject = ({
         width={1200}
         height={900}
         alt="Capa"
-        className=" h-52 object-contain w-[45%] max-md:h-auto py-2"
+        className=" h-52 max-w-80 object-contain w-[45%] max-md:h-auto py-2"
         priority
       />
       <div className="flex flex-col items-center gap-6 w-[50%] max-md:w-full max-md:px-2 max-md:mb-2 max-md:text-center">
