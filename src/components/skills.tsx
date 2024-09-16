171eea9a0663945a93e892c2f03c9fa01b6bb3a0
andre <andrealbuquerque2001@gmail.com>
2024-09-16 11:34:23 -0300

    feat: update component

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,30 +1,30 @@
 export const Skills = () => {
   return (
-    <div className="text-white mt-3 space-y-3 mb-8">
-      <h1 className="text-xl">My skills</h1>
+    <div className="text-zinc-400 mt-3 space-y-3 max-md:pb-8">
+      <h1 className="text-xl">Minhas habilidades</h1>
       <div className="grid grid-cols-2 max-sm:grid-cols-1">
         <div className="">
           <h3 className="">Front-end</h3>
           <div className="flex flex-wrap gap-2">
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               HTML
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               CSS
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               JavaScript
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Typescript
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               React
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Next.Js
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Tailwind
             </span>
           </div>
@@ -32,36 +32,39 @@ export const Skills = () => {
         <div>
           <h3 className="">Back-end</h3>
           <div className="flex flex-wrap gap-2">
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               PHP
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Laravel
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Java
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Spring Boot
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Python
+            </span>
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
+              Node
             </span>
           </div>
         </div>
         <div>
           <h3 className="">Database</h3>
           <div className="flex flex-wrap gap-2">
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               SQL
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               MySql
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               MariaDB
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               PostgreSQL
             </span>
           </div>
@@ -69,22 +72,22 @@ export const Skills = () => {
         <div>
           <h3 className="">Others</h3>
           <div className="flex flex-wrap gap-2">
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Docker
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Figma
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               VS code
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Linux
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Postman
             </span>
-            <span className="px-4 py-2  bg-blue-500 text-white rounded-md">
+            <span className="px-4 py-2 bg-blue-900 opacity-80 text-white rounded-md">
               Swagger
             </span>
           </div>
